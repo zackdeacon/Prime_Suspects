d9e83241ae5afb099b2cbca09a5a0b063b012cd7
Zachary Deacon <zackdeacon347@gmail.com>
2020-07-23 11:21:23 -0700

    database functionality

--- server.js
+++ server.js
@@ -1,34 +1,31 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
 var express = require("express");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
 var db = require("./models");
 
-// Sets up the Express app to handle data parsing
+var app = express();
+
+app.use(express.static("public"));
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory
-app.use(express.static("public"));
+var exphbs = require("express-handlebars");
 
-// Routes
-// =============================================================
-require("./routes/api-routes.js")(app);
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
+var routes = require("./controllers/controller.js");
+
+app.use(routes);
+
+
+var PORT = process.env.PORT || 4347;
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("App now listening on port:", PORT);
   });
 });
