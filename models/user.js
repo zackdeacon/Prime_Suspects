023d561adc2ebafcfb7acc2ef1c4e62458ec27dc
Cody Samuels <twowoodenspoons@gmail.com>
2020-07-23 16:12:48 -0700

    Got the postman to add to the user database.

--- models/user.js
+++ models/user.js
@@ -1,29 +1,28 @@
-module.exports = function(sequelize, DataTypes) {
-    var user = sequelize.define("user", {
-        name: DataTypes.STRING,
-        username: DataTypes.STRING,
-        password: DataTypes.STRING,
-        email: DataTypes.STRING,
-        address: DataTypes.STRING,
-        city: DataTypes.STRING,
-        state: DataTypes.STRING,
-        zip: DataTypes.STRING,
-        phoneNumber: DataTypes.STRING,
-      },{
-          tableName: "user"
-      })
-    
-      user.associate = function(models) {
-        // Associating Author with Posts
-        // When an Author is deleted, also delete any associated Posts
-        user.hasOne(models.cart, {
-          onDelete: "cascade"
-        });
-      };
-    
+module.exports = function (sequelize, DataTypes) {
+  var user = sequelize.define("user", {
+    email: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    address: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    zip: DataTypes.STRING,
+    phoneNumber: DataTypes.STRING,
+  }, {
+    tableName: "user"
+  })
 
-    //   user.sync();
-    
-    return user;
-  }
-  
+  user.associate = function (models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    user.hasOne(models.cart, {
+      onDelete: "cascade"
+    });
+  };
+
+
+  //   user.sync();
+
+  return user;
+}
