6351efdf2a4dec7cdfe523ecfad641744d144dc6
Cody Samuels <twowoodenspoons@gmail.com>
2020-07-23 11:48:09 -0700

    store setup initialization

--- controllers/controller.js
+++ controllers/controller.js
@@ -18,6 +18,35 @@ router.get("/", function(req, res) {
 
 });
 
+router.get('/public-keys', (req, res) => {
+  res.send({ key: process.env.STRIPE_PUBLISHABLE_KEY})
+})
+
+router.post('/my-route', (req, res) => {
+  console.log('body', req.body)
+  // PUT DATA IN DB
+  res.send(req.body);
+})
+
+router.post('/webhook', (req, res) => {
+  const event = req.body;
+
+  switch(event.type) {
+    case 'checkout.session.completed':
+      const session = event.data.object;
+      console.log("Checkout Session ID: ", session.id)
+      break;
+
+    case 'payment_intent.created':
+      const paymentIntent = event.data.object
+      console.log('PaymentIntent Created ', paymentIntent.id)
+      break;
+
+    default:
+      console.log('Unknown event type: ' + event.type)
+  }
+  res.send({ message: 'success' });
+})
 
 // router.get("/burgers", function(req, res) {
 
