7b7c21b319e1891e90101efb93ad423cf5bf0ca4
Cody Samuels <twowoodenspoons@gmail.com>
2020-07-25 10:02:20 -0700

    Navbar now uses user's name

--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,18 @@
-console.log("Hello!")
 $("#loginForm").submit(function(event){
     event.preventDefault();
     const userObj = {
         email:$("#loginEmail").val(),
         password:$("#loginPassword").val()
     }
-    console.log(userObj);
+    // console.log(userObj);
     $.ajax({
-        url:"/auth/login",
+        url:"/login",
         method:"POST",
         data: userObj
     }).done(function(data){
         console.log(data);
         // alert('logged in!');
-        // location.href = "/"
+        location.href = "/"
     }).fail(function(err){
         console.log(err);
         // alert("something went wrong!")
