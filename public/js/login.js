2fe2eb31fc25dbb2f6b6ee74d012f4db198bbafc
Cody Samuels <twowoodenspoons@gmail.com>
2020-07-24 18:34:24 -0700

    Basic JS down

---
+++ public/js/login.js
@@ -0,0 +1,22 @@
+$("#loginForm").submit(function(event){
+    event.preventDefault();
+    const userObj = {
+        email:$("#loginEmail").val(),
+        password:$("#loginPassword").val()
+    }
+    // console.log(userObj);
+    $.ajax({
+        url:"/auth/login",
+        method:"POST",
+        data: userObj
+    }).done(function(data){
+        console.log(data);
+        // alert('logged in!');
+        location.href = "/"
+    }).fail(function(err){
+        console.log(err);
+        // alert("something went wrong!")
+        location.reload();
+        $("#errorspan").text("Incorrect login info.")
+    })
+})
