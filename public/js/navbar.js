2fd6ccaea51e2e8b1b32ee5ec28cf523418d7b89
Cody Samuels <twowoodenspoons@gmail.com>
2020-07-25 20:25:36 -0700

    Completed the login button. Not super happy with it

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,5 +2,12 @@ $.ajax('/readsessions').done(function(data){
     console.log(data);
     if(data.user){
         $("#navbarUser").text(`Hello, ${data.user.name}`)
+    } else {
+        $("#nav-mobile").empty()
+        const link = $("<a>")
+        link.attr("href", "/login")
+        link.text("Login")
+        $("#nav-mobile").attr("class", "right")
+        $("#nav-mobile").append(link)
     }
 })
