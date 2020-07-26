a5783de4fe36d9b5764d9824ce1faaac4a74bb70
Cody Samuels <twowoodenspoons@gmail.com>
2020-07-25 22:35:04 -0700

    created cart.js. cleaned up styling a little.

---
+++ public/js/cart.js
@@ -0,0 +1,15 @@
+// NOT QUITE WORKING YET
+console.log("Hello world!")
+// $(document).ready(function () {
+//     $(".zackSubmit").on("click", function () {
+//         let clickedId = $(this).attr("data-id");
+//         $.ajax({
+//             url: `/cartRoute/${clickedId}`,
+//             method: "DELETE",
+//             data: itemId 
+//         }).done(function (data) {
+//             console.log(data);
+//             location.reload();
+//         })
+//     })
+// })
