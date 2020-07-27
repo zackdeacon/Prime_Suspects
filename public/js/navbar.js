a4e9235622ff159c735d64c62269f47d5348c5ca
wild1k <cssndrchmbrln@gmail.com>
2020-07-27 16:43:28 -0700

    saving to checkout to dev

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -3,11 +3,11 @@ $.ajax('/readsessions').done(function(data){
     if(data.user){
         $("#navbarUser").text(`Hello, ${data.user.name}`)
     } else {
-        $("#nav-mobile").empty()
+        $("#navList").empty()
         const link = $("<a>")
         link.attr("href", "/login")
         link.text("Login")
-        $("#nav-mobile").attr("class", "right")
-        $("#nav-mobile").append(link)
+        $("#navList").attr("class", "right")
+        $("#navList").append(link)
     }
 })
