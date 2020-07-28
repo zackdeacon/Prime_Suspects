dac50cf3906e811351a26934d66056be446cb9f7
Zachary Deacon <zackdeacon347@gmail.com>
2020-07-28 13:46:15 -0700

    adding seed route

--- server.js
+++ server.js
@@ -27,6 +27,29 @@ app.engine("handlebars", exphbs({
 }));
 app.set("view engine", "handlebars");
 
+
+app.get("/seeding",(req,res)=>{
+  const csv = require('csv-parser');
+const fs = require('fs');
+var db = require("./models");
+let csvData = [];
+fs.createReadStream('DatafinitiElectronicsProductsPricingData.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    csvData.push(row);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+    db.item.bulkCreate(csvData).then(function(data){
+        // console.log(data[0]);
+    }).catch(err =>  console.log(err));
+  // db.item.create(csvData[0]).then(function(data){
+  //       console.log(data);
+  //   }).catch(err =>  console.log(err));
+     
+  });
+})
+
 const userCreateRoutes = require("./controllers/userCreateController.js");
 app.use(userCreateRoutes);
 
