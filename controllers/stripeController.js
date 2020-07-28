18385c511235d786b2701b5b7986986b0108d2d3
Cody Samuels <twowoodenspoons@gmail.com>
2020-07-27 19:22:44 -0700

    pre merge commmit

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -3,10 +3,18 @@ const router = express.Router();
 require('dotenv').config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
+const db = require("../models/");
+
 // router.get('/checkout', async (req, res) => {
 //   const session = // ... Fetch or create the Checkout Session
 //   res.render('checkout', { session_id: session.id });
 // });
+
+router.get('/config', async (req, res) => {
+  res.send({
+    publicKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
 
 router.get('/checkout-session', async (req, res) => {
   const { sessionId } = req.query;
@@ -14,63 +22,64 @@ router.get('/checkout-session', async (req, res) => {
   res.send(session);
 });
 
-// router.post('/create-checkout-session', async (req, res) => {
-//   const domainURL = process.env.DOMAIN;
-//   db.cart.findOne({
-//     where: {
-//       id: req.params.id
-//     }, include: [db.item]
-//   }).then()
-//   const session = await stripe.checkout.sessions.create({
-//     payment_method_types: ['card'],
-//     line_items: [],
-//     mode: 'payment',
-//     // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
-//     success_url: `${domainURL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
-//     cancel_url: `${domainURL}/canceled.html`,
-//   });
-
-//   res.send({
-//     sessionId: session.id,
-//   });
-// })
-
-router.get('/config', async (req, res) => {
-  // const price = await stripe.prices.retrieve(process.env.PRICE);
-
-  res.send({
-    publicKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    unitAmount: price.unit_amount,
-    currency: price.currency,
-  }).catch(err =>{
-    console.log(err)
-  })
-});
-
 router.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
-    line_items: [{
-      price_data: {
-        currency: 'usd',
-        product_data: {
-          name: 'T-shirt',
-        },
-        unit_amount: 2000,
-      },
-      quantity: 1,
-    }],
-    mode: 'payment',
-    success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-    cancel_url: 'https://example.com/cancel',
-  });
+  const domainURL = process.env.DOMAIN;
+  db.cart.findOne({
+    where: {
+      cartId: req.session.user.cartId
+    }, include: [db.item]
+  }).then(function (cartitems) {
+    console.log(cartitems)
+    // const session = await stripe.checkout.sessions.create({
+    //   payment_method_types: ['card'],
+    //   line_items: [],
+    //   mode: 'payment',
+    //   // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+    //   success_url: `${domainURL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
+    //   cancel_url: `${domainURL}/canceled.html`,
+    // });
 
-  res.send({
-    sessionId: session.id,
-  });
+    res.send({
+      sessionId: session.id,
+    });
+  })
+})
+
+router.post('/webhook', async (req, res) => {
+  let data;
+  let eventType;
+  // Check if webhook signing is configured.
+  if (process.env.STRIPE_WEBHOOK_SECRET) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    let signature = req.headers['stripe-signature'];
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    // Extract the object from the event.
+    data = event.data;
+    eventType = event.type;
+  } else {
+    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+    // retrieve the event data directly from the request body.
+    data = req.body.data;
+    eventType = req.body.type;
+  }
+
+  if (eventType === 'checkout.session.completed') {
+    console.log(`🔔  Payment received!`);
+  }
+
+  res.sendStatus(200);
 });
-
-
 // EXPORT
 // ===============================================================
 module.exports = router;
--- public/js/cart.js
+++ public/js/cart.js
@@ -1,6 +1,6 @@
 // NOT QUITE WORKING YET
 $(document).ready(function () {
-    const stripe = Stripe('pk_test_51H6lyMACrjNtDH8GqBanKtwFegjiWxVci5kU3I8kXSc0gtl4hZg32JkxMpxobsCoJRyFKuR58V0KgdNwPLjLenpy009kCobCkO');
+    var stripe = Stripe('pk_test_51H6lyMACrjNtDH8GqBanKtwFegjiWxVci5kU3I8kXSc0gtl4hZg32JkxMpxobsCoJRyFKuR58V0KgdNwPLjLenpy009kCobCkO');
     // $(".zackSubmit").on("click", function () {
     //     let clickedId = $(this).attr("data-id");
     //     $.ajax({
@@ -12,6 +12,8 @@ $(document).ready(function () {
     //         location.reload();
     //     })
     // })
+
+
 
     // function deleteItem() {
     //     event.preventDefault();
@@ -26,104 +28,53 @@ $(document).ready(function () {
     //         location.reload();
     //     })
     // }
+    
+    const checkoutButton = $('.checkout-button');
+    cartObj =
+    {
+        payment_method_types: ['card'],
+        line_items: [],
+        mode: 'payment',
+        success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url: 'https://example.com/cancel'
+    }
 
-    // const createCheckoutSession = function () {
-    //     return fetch('/create-checkout-session', {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //     }).then(function (result) {
-    //       return result.json();
-    //     });
-    //   };
+    function getCartID() {
 
-    // const checkoutButton = $('.checkout-button');
-    // cartObj =
-    // {
-    //     payment_method_types: ['card'],
-    //     line_items: [],
-    //     mode: 'payment',
-    //     success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-    //     cancel_url: 'https://example.com/cancel',
-
-    // }
-
-    // function getCartID() {
-
-    //     $.ajax('/readsessions').done(function (data) {
-    //         console.log(data);
-    //         cartId = data.user.cartId
-    //         readCart(cartId)
-    //     })
-
-    //     function readCart(cartId) {
-    //         $.ajax({
-    //             url: `/api/carts/${cartId}`,
-    //             method: "GET"
-    //         }).done(function (cartInfo) {
-    //             for (let i = 0; i < cartInfo.items.length; i++) {
-    //                 cartObj.line_items.push(
-    //                     {
-    //                         price_data: {
-    //                             currency: 'usd',
-    //                             product_data: {
-    //                                 name: cartInfo.items[i].name
-    //                             },
-    //                             unit_amount: cartInfo.items[i].prices_amountMax,
-    //                         },
-    //                         quantity: 1,
-    //                     },
-    //                 )
-    //             }
-    //             console.log(cartInfo);
-    //             console.log(cartObj)
-    //         }).fail(err => {
-    //             console.log(err)
-    //         })
-    //     }
-
-    // }
-    // getCartID();
-
-
-
-    var createCheckoutSession = function () {
-        // var inputEl = document.getElementById('quantity-input');
-        var quantity = 1
-      
-        return fetch('/create-checkout-session', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            quantity: quantity,
-            locale: i18next.language.toLowerCase().split('-')[0],
-          }),
-        }).then(function (result) {
-          return result.json();
-        });
-      };
-      
-      /* Get your Stripe publishable key to initialize Stripe.js */
-      fetch('/config')
-        .then(function (result) {
-          return result.json();
+        $.ajax('/readsessions').done(function (data) {
+            cartId = data.user.cartId
+            readCart(cartId)
         })
-        .then(function (json) {
-          window.config = json;
-          var stripe = Stripe(config.publicKey);
-          updateQuantity();
-          // Setup event handler to create a Checkout Session on submit
-          document.querySelector('#checkout-button').addEventListener('click', function (evt) {
-            createCheckoutSession().then(function (data) {
-              stripe
-                .redirectToCheckout({
-                  sessionId: data.sessionId,
-                })
-                .then(handleResult);
-            });
-          });
-        });
+
+        function readCart(cartId) {
+            $.ajax({
+                url: `/api/carts/${cartId}`,
+                method: "GET"
+            }).done(function (cartInfo) {
+                for (let i = 0; i < cartInfo.items.length; i++) {
+                    cartObj.line_items.push(
+                        {
+                            price_data: {
+                                currency: 'usd',
+                                product_data: {
+                                    name: cartInfo.items[i].name
+                                },
+                                unit_amount: cartInfo.items[i].prices_amountMax,
+                            },
+                            quantity: 1,
+                        },
+                    )
+                }
+                // console.log(cartInfo);
+                console.log(cartObj)
+            }).fail(err => {
+                console.log(err)
+            })
+        }
+
+    }
+    getCartID();
+
+
+    // const session = await stripe.checkout.sessions.create(cartObj);
 })
